505ccb477ed7002de7989e486496eaafc9c72ae1
Matthew Hill <matthill82@live.com>
2014-10-26 19:01:02 +0000

    restructure of app

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+	// the database url to connect mongolab
+	url : 'mongodb://mhill168:Monday11@ds063789.mongolab.com:63789/weddingdb'
+}
--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-/**
- * Created by mhill168 on 24/10/14.
- */
-
-// set up ========================
+// set up ======================================================================
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
-var morgan = require('morgan'); 			// log requests to the console (express4)
+var port  	 = process.env.PORT || 8080; 				// set the port
+var database = require('./config/database'); 			// load the database config
+
+var morgan = require('morgan'); 		// log requests to the console (express4)
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-// trust the proxy
-app.enable('trust proxy');
-
-// configuration =================
-mongoose.connect('mongodb://mhill168:Monday11!>@proximus.modulusmongo.net:27017/wutaQ6eh'); 	// connect to mongoDB database on modulus.io
+// configuration ===============================================================
+mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); 										// log every request to the console
@@ -23,77 +19,9 @@ app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-// Model =======================
-var Todo = mongoose.model('Todo', {
-	text : String
-});
-
-/**
- * HTTP Verb   URL	    Description
-	 GET	/api/todos	Get all of the todos
-	 POST	/api/todos	Create a single todo
-	 DELETE	/api/todos/:todo_id	Delete a single todo
- **/
-
-// Routes =====================
-// api ------------------------
-// get all todos
-app.get('/api/todos', function (req, res) {
-
-	// use mongoose to get all the todos in the database
-	Todo.find(function (err, todos) {
-
-		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-		if(err) res.send(err);
-
-		res.json(todos); // return all todos in json format
-
-	});
-});
-
-// application ----------------
-app.get('*', function (req, res) {
-	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
-
-// create todo and send back all todos after creation
-app.post('/api/todos', function (req, res) {
-
-	// create todo, information comes from an ajax request from angular
-	Todo.create({
-		text : req.body.text,
-		done : false
-	}, function (err, todo) {
-
-		if(err) res.send(err);
-
-		// get and return all the todos after you create another
-		Todo.find(function (err, todos) {
-			if(err) res.send(err);
-
-			res.json(err);
-		})
-	})
-});
-
-// delete a todo
-app.delete('/api/todos/:todo_id', function(req, res) {
-	Todo.remove({
-		_id : req.params.todo_id
-	}, function(err, todo) {
-		if (err)
-			res.send(err);
-
-		// get and return all the todos after you create another
-		Todo.find(function(err, todos) {
-			if (err)
-				res.send(err)
-			res.json(todos);
-		});
-	});
-});
-
+// routes ======================================================================
+require('./app/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
-app.listen(8080);
-console.log("App listening on port 8080");
+app.listen(port);
+console.log("App listening on port " + port);
